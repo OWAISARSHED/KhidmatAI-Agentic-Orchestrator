0483308bf91863631f943a6e88659f48afde8bc8
OWAISARSHED <167348452+OWAISARSHED@users.noreply.github.com>
2026-05-17 01:18:47 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/khidmatai/MainActivity.java
+++ app/src/main/java/com/example/khidmatai/MainActivity.java
@@ -24,14 +24,7 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String PROJECT_ID = "serviceorchestrator-496409";
-    private static final String CLIENT_EMAIL = "vertex-express@serviceorchestrator-496409.iam.gserviceaccount.com";
-
-    // Clean and standard formatted single line key block to avoid bad base-64 parsing breaks
-    private static final String PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNnQiOlVrCFHQG\neA5o4717LDFyIJ3FUtIEgHsZlofffH7JBw1AGsWiAV10knacYCA/ziys10moiSFm\nlgDcGWmWHWKdVwN2udLBN7g8Gkc7uh3MQV4Y10HfsKWCJC2eJlGIFbQo4ntmoknE\nn37i8FHTFw4p09NKnkbXMkoGibEJ6Tw7Og/RxLkFM/6E69ESM/fJAedBuUGpy+Fs\n5lTb/b75WC+faNGo/tj9+NC73gPweLXJCoyhLW+uUFzC9Mq+PAew1qQgdUCIPqN1\nk4Ii3wnEMF4BvpyYvaYNWHM6puuoAgk9HnZa4BBM7sKXNHiUw42Jm9lqxITdrF/3\nvZ2nocylAgMBAAECggEAKhyvT7q10bnMMLX+p4t7d8KLpwcYoT1O//aLn+hFkGeo\ndQZxd490KnPynulPi0Qh3secvVLDxpW9hkwTRYEb78+5bqXPcw3opUPDA2ZTcB90\nsmyx988Asi61G1RLFpDdEoz1bwlCAnmbN77+Bz921s/qSfVVWXtU4Ybug+vIG5Hw\nVeLs1hbiruGMGaqqTNX56GIodahJE25P5hpU+slRMnk8PE6jvnUEG/8R4ry86Kb9\nwiiMn5pWPinRerTp+h60w9detE8Mm86uzQL9CKhFTsOG7SkrZy5osIIHWegcJmgk\nA7ft4vE8h4jbN+qRTf8KgVA0QXgHLJMB7weBGrPCkQKBgQDly+ZxTuZXgjrWL3cv\nY+LNOw0W+i3Qom43uujxb/xTRMJaJ8ZyEr8p5hapD1KpI+0bgfKPxVWoFviZ70TB\n3r7smquKPoJ3UKmwzh7BJc69XN1HfJa0470j8IcX0ZFKN7oIMSttC5O5QEtFIrB8\nxauL0j6dOpnDD7b/hXgxUaCucQKBgQDlDzGK6hK23Ru+wsp2Hseqgwu6GO2FJU05\n2Txbq+33rfWCgy4W/yUmDO2HajMf1vciAZbP0mXvvOnnLYdek91E/yLgGawWWiij\n34SLcNqmORZguxl3YA72bONJm6BU1JcWifIK6NS9TJvPB+T+kYSG49Th8foe104d\nQmprpnLDdQKBgCCt5o+mkMtREdzz0xdCNTbKdwoVef55mwMB6a6sJk4RMTJxBZ+j\nWtwyhHZ2X2L+YPHQgRZBEfVdju0VQcnwS2aKeGqoiRU+u2MRbP46yjPTyQQpi6Ud\n3gAGbK/OdcMAOcyAcZSIWPG3Wvzx+fdEhSjVNWJWuxcqXPPpnzYkTZ2BAoGBAMZZ\n7M3dOk1tlKwQ4997ce0+w3XoRi4JCiGw8upzLINQ+YuF6vBxCwszILEljpXgwWqK\nM71M9YavcWEF9PY6Vt57jQjAUlucwRp+4LkbBcvxHWawPjI4VVyhGATSpfkvipIy\n7Az0Z9xwrW+O54ZmHnvsaJkrwCrayMLfk2Ya4cBNAoGAYCeNGER0t1WWTWlU3/yH\nOQ7C29U+GuCUS52zrREOG+gT+HVK29MYqErkdMzdK6mc3KADTVSaBljWjrrZcR8i\ngKg6HfZi/zY0ln3spU4PSyWQKezgOTYDYjPulnd1MmN/cpYfjfuXT6EKBK5NCvB6\nsz2sNrJQELl5zIwDpg/RaWY=\n-----END PRIVATE KEY-----\n";
-
-    private static final String API_URL = "https://us-west1-aiplatform.googleapis.com/v1/projects/" + PROJECT_ID + "/locations/us-west1/agents/Orchestrator_Agent:sessions/session1:detectIntent";
-
+    
     private EditText etRequest;
     private androidx.cardview.widget.CardView cvResultFrame;
     private Button btnSearch;
